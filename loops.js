412e6a364bffd55a3cecc484797e0bf6f691ccaf
Matthew Linderman <mwl53@mit.edu>
2018-01-16 17:16:01 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loops.`)
+  }
+  return array
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(number)
+    number--
+  }
+  return `done`
+}
